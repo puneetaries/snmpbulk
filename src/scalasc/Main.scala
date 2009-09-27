be8f6498d200b27ea102c37f0458908b807fb3df
Steve <Steve@flanagans>
2009-09-27 01:39:00 -0500

    proper printing in async as is there in sync version

--- src/scalasc/Main.scala
+++ src/scalasc/Main.scala
@@ -41,33 +41,6 @@ object Main {
               new OID("1.3.6.1.2.1.2.2.1.10"), // in octects
               new OID("1.3.6.1.2.1.2.2.1.16"), // out octects
         )
-
-        var tl = new TableListener() {
-            private var finished = false
-
-            override def isFinished(): boolean = { finished }
-            
-            override def finished(event:TableEvent ) : Unit = {
-               finished = true;
-               event.getUserObject().synchronized {
-                  event.getUserObject().notify();
-               }
-            }
-      
-            override def next(event:TableEvent) : boolean = {
-                System.out.println("tick last")
-                System.out.println(event)
-                true
-            }
-        }
-        println("setup")
-        var o = new Object();
-        utils.getTable(target, columns, tl, o, null, null)
-        println("waiting")
-        while (tl.isFinished()) {
-           o.wait();
-         }
-        println("wait finish")
         var table = new LinkedList[TableEvent](utils.getTable(target, columns, null, null).asInstanceOf[java.util.LinkedList[TableEvent]])
         //var table = utils.getTable(target, columns, null, null).asInstanceOf[LinkedList[TableEvent]]
   
@@ -124,7 +97,20 @@ object Main {
 
             override def next(event:TableEvent) : boolean = {
                 System.out.println("tick last")
-                System.out.println(event)
+                if ( event.getColumns()!=null )
+                    for ( vb:VariableBinding <- event.getColumns() )
+                    {
+                        print("{ " + vb.getOid())
+                        //+ " = " + vb.getVariable().toString() +
+                        val v = vb.getVariable()
+                        v match {
+                            case vx:OctetString => print(" \"" + vx.toASCII('\0') + "\" ")
+                            case _ => print (" " + v + " ")
+                        }
+                        print(" } ")
+                    }
+                else
+                    System.err.println("err: " + event.getErrorMessage())
                 true
             }
         }
